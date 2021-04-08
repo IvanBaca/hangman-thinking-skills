971dbe139420b156c9e5170990b41f40ec5476a2
IvanBaca <ivan.videogamer@gmail.com>
2021-04-08 03:21:13 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const numIntentos = document.getElementById("attempts-num");
+const hangmanDiv = document.getElementById("hangman-div");
+const submitButton = document.getElementById("submit-button");
+const wordGuessDiv= document.getElementById("word-guess-div");
+const guessInput= document.getElementById("guess-input");
+
+var word = "hangman";
+var guessed = "#######";
+var guesses = 0;
+
+function gameStart(){
+    wordGuessDiv.innerHTML=guessed;
+}
+
+function cycle(){
+    let submittedAttempt = guessInput.value;
+    if(submittedAttempt.length==1){
+        if(guesses<6){
+            let coincidence = false;
+            for(let i=0;i<word.length;i++){
+                if(word.charAt(i)==submittedAttempt){
+                    guessed = guessed.replaceAt(i,word.charAt(i));
+                    console.log(guessed);
+                    coincidence=true;
+                }
+            }
+            if(!coincidence){
+                guesses++;
+            }
+            wordGuessDiv.innerHTML=guessed;
+        }
+    }else{
+        alert("Debe entrar solo una letra");
+    }
+    if(guessed===word){
+        //Load victory screen
+    }
+    if(guesses>5){
+        //Load fail screen
+    }
+}
+
+String.prototype.replaceAt = function(index, replacement) {
+    return this.substr(0, index) + replacement + this.substr(index + replacement.length);
+}
+
+gameStart();
+
+submitButton.addEventListener("click", cycle);
